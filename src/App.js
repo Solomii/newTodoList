6cdc751d9db152f7e10835fc1f3c3b3e635d6e2a
Solomii <kushniryksolomiya@gmail.com>
2021-01-12 12:25:54 +0200

    added uuid, router, route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import "./App.css";
-import Todos from "./components/Todoes";
-import Header from "./components/layout/Header";
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Todos from "./components/Todoes";
+import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
+import Header from "./components/layout/Header";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: "Take out the trash",
         completed: false,
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Dinner",
         completed: false,
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: "Meeting with boss",
         completed: false,
       },
@@ -43,17 +47,40 @@ class App extends Component {
     });
   };
 
+  //Add todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      complete: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
   render() {
     // console.log(this.state.todos);
     return (
-      <div className="App">
-        <Header />
-        <Todos
-          todos={this.state.todos}
-          markComplete={this.markComplete}
-          delTodo={this.delTodo}
-        />
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
